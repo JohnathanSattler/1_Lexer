8312468be08190ebe7bfced4fbc0262f00697010
Johnathan Sattler <jsattler@outlook.com>
2016-09-27 20:08:21 -0400

    Initial Commit

---
+++ lexer.c
@@ -0,0 +1,16 @@
+// Compiler Builder 8
+// Lester Young
+// Timothy Konzel
+// Remington Howell
+// Johnathan Sattler
+// John Herold
+
+// Included libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+
+	return 0;
+}
