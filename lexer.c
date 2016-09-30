9512d1a6a08d2ed17b2eaeb91f8933d65402988c
Tim Konzel <timkonzel@hotmail.com>
2016-09-30 13:12:19 -0400

    Revert "attempting to keep track of line and col num"
    This reverts commit 758365ae5cd23567feacff95edee815f612a75c0.

--- lexer.c
+++ lexer.c
@@ -15,26 +15,8 @@
 int main(int argc, const char * argv[]) {
 
 	sourceCode * code = NULL;
-
+	
 	handleInput(argc, argv, code);
 
-    lexTest(code);
 	return 0;
-}
-
-void lexTest(sourceCode *  code) {
-    if (code == NULL)
-        return;
-    int lineNum, colNum = 0;
-    while (code->c != NULL) {
-        switch (code->c) {
-        case '\n':
-            lineNum++;
-            colNum = 0;
-            break;
-        }
-        colNum++;
-        code = code->next;
-    }
-
 }
